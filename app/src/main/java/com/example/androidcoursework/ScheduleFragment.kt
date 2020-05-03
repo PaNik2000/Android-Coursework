fe86758bd76ac76fb0f5b03b9fe19e66b5f83fff
PaNik2000 <arshakov-nikita@mail.ru>
2020-05-03 14:50:04 +0300

    Customized schedule fragment

--- app/src/main/java/com/example/androidcoursework/ScheduleFragment.kt
+++ app/src/main/java/com/example/androidcoursework/ScheduleFragment.kt
@@ -1,13 +1,23 @@
 package com.example.androidcoursework
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_schedule.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ScheduleFragment : Fragment() {
+
+    private val scheduleList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +29,42 @@ class ScheduleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         Log.d("abc", "schedule fragment")
-        return inflater.inflate(R.layout.fragment_schedule, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_schedule, container, false)
+
+        val listView = view.findViewById(R.id.scheduleList) as ListView
+
+        scheduleList.add("9:00 - 10:30")
+        scheduleList.add("10:40 - 12:10")
+        scheduleList.add("13:10 - 14:40")
+        scheduleList.add("14:50 - 16:20")
+        scheduleList.add("16:30 - 18:00")
+
+        listView.adapter = ScheduleListAdapter(activity as Context, R.layout.schedule_list_element, scheduleList)
+        listView.setOnItemClickListener(object : AdapterView.OnItemClickListener {
+            override fun onItemClick(parent: AdapterView<*>, itemClicked: View, position: Int, id: Long) {
+                //TODO intent
+            }
+        })
+
+        return view
+    }
+}
+
+class ScheduleListAdapter(context : Context, val resource: Int, objects: MutableList<String>)
+    : ArrayAdapter<String>(context, resource, objects){
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val schedule = getItem(position)
+        var view = convertView
+
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(resource, null)
+        }
+
+        (view?.findViewById(R.id.scheduleID) as TextView).text = (position + 1).toString()
+        (view.findViewById(R.id.scheduleTime) as TextView).text = schedule
+
+        return view
     }
 }
